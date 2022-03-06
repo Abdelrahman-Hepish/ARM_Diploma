fec700aa1dd192fb1b0bd1307da3fc89fa6a9816
Abdelrahman Hepish <Abdelrahman.hepish2001@gmail.com>
2022-03-06 03:04:35 +0200

    Led Matrix driver primitive one , S : working

---
+++ 04_APP/07_TryTough_LEDMATRIX/src/main.c
@@ -0,0 +1,28 @@
+/*
+ * main.c
+ *
+ *  Created on: Dec 12, 2021
+ *      Author: Abdelrahman Hepish
+ */
+#include "..\..\..\01_LIB\01_STD_TYPES\STD_TYPES.h"
+#include "..\..\..\01_LIB\02_BIT_MATH\BIT_MATH.h"
+#include "..\..\..\02_MCAL\STM32F103C\01_RCC\RCC_interface.h"
+#include "..\..\..\02_MCAL\STM32F103C\02_GPIO\GPIO_interface.h"
+#include "..\..\..\02_MCAL\STM32F103C\07_STK\STK_interface.h"
+#include "..\..\..\03_HAL\02_LEDMRX\LEDMRX_interface.h"
+void main(void)
+{
+	/* initialize clk for system */
+	RCC_voidInitSystemClk(RCC_PLL_OFF_HSI_OFF_HSE_ON_BYPASS_OFF,RCC_SYSTCLK_HSE,RCC_PLL_NOT_USED ,PLL_IN_X_1) ;
+	/* initialize gpioa clk */
+	RCC_voidSetPeripheralClkState(RCC_APB2_IOPA,RCC_PERIPHERAL_ENABLE) ;
+	RCC_voidSetPeripheralClkState(RCC_APB2_IOPB,RCC_PERIPHERAL_ENABLE) ;
+    LEDMRX_voidInit() ;
+    u8 A[8] = {0, 0, 124, 18, 18, 124, 0, 0} ;
+	while(1)
+	{
+    LEDMRX_voidDisplay(A) ;
+	}
+}
+
+
