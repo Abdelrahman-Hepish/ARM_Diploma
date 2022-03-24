472605b5c39a9d59e59287eed7a88df91d4c6916
Abdelrahman Hepish <Abdelrahman.hepish2001@gmail.com>
2022-03-24 12:32:22 +0200

    Creating Services layer "Scheduler skeleton"

---
+++ 05_SERV/01_SCHEDULER/SCHEDULER_config.h
@@ -0,0 +1,11 @@
+/*************************************************************************/
+/* Author        : Abdelrahman Hepish                                    */
+/* File          : SCHEDULER_config.h                                    */
+/* Date          : 24 MARCH 2021                                         */
+/* Version       : V01                                                   */
+/* GitHub        : https://github.com/Abdelrahman-Hepish                 */
+/*************************************************************************/
+#ifndef __SCHEDULER_CONFIG_H__
+#define __SCHEDULER_CONFIG_H__
+		
+#endif // __SCHEDULER_CONFIG_H__
---
+++ 05_SERV/01_SCHEDULER/SCHEDULER_program.c
@@ -0,0 +1,12 @@
+/*************************************************************************/
+/* Author        : Abdelrahman Hepish                                    */
+/* File          : SCHEDULER_program.c                                   */
+/* Date          : 24 MARCH 2021                                         */
+/* Version       : V01                                                   */
+/* GitHub        : https://github.com/Abdelrahman-Hepish                 */
+/*************************************************************************/
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+#include "SCHEDULER_interface.h"
+#include "SCHEDULER_private.h"
+#include "SCHEDULER_config.h"
