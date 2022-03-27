862b58d9281384d431150d466d13515411afd765
Abdelrahman Hepish <Abdelrahman.hepish2001@gmail.com>
2022-03-27 06:46:13 +0200

    Basic DAC driver finished , S::W

--- 03_HAL/05_DAC/DAC_program.c
+++ 03_HAL/05_DAC/DAC_program.c
@@ -10,3 +10,21 @@
 #include "DAC_interface.h"
 #include "DAC_private.h"
 #include "DAC_config.h"
+#include "SONG.h"
+#include "STK_interface.h" 
+#include "GPIO_interface.h"
+static void DAC(void) ; 
+u32 currentSample ; 
+
+void DAC_voidInit(void) 
+{
+	u32 sampleTicks = 1000000 / AUDIO_RATE ; 
+	STK_voidInit(STK_ENABLED,STK_CLKSRC_AHB_BY_8,STK_INT_ENABLE) ;
+	GPIO_voidInitByte(DAC_PORT,DAC_BYTE,PIN_NM_OUTPUT_2MHZ,0X00) ;
+	STK_voidSetIntervalPeriodic(sampleTicks,DAC) ;
+}
+static void DAC(void)
+{
+	GPIO_voidSetByteState(DAC_PORT,DAC_BYTE,Song_raw[currentSample]) ;
+	currentSample = (currentSample+1)%AUDIO_SIZE ;
+}
